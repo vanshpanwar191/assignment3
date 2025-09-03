5be191477f99fdb0dbb82e84e658b2bf7c04709a
vanshpanwar191 <vpanwar_be24@thapar.edu>
2025-09-03 14:46:28 +0530

    Update Q3.cpp

--- Q3.cpp
+++ Q3.cpp
@@ -1,2 +1,46 @@
-#include<iostream>
+#include <iostream>
+#include <stack>
 using namespace std;
+
+bool isBalanced(string expr) {
+    stack<char> s;
+
+    for (char ch : expr) {
+        
+        if (ch == '(' || ch == '{' || ch == '[') {
+            s.push(ch);
+        }
+        
+        else if (ch == ')' || ch == '}' || ch == ']') {
+            if (s.empty()) return false; 
+
+            char top = s.top();
+            s.pop();
+
+            
+            if ((ch == ')' && top != '(') ||
+                (ch == '}' && top != '{') ||
+                (ch == ']' && top != '[')) {
+                return false;
+            }
+        }
+    }
+
+  
+    return s.empty();
+}
+
+int main() {
+    string expr;
+    cout << "Enter an expression: ";
+    cin >> expr;
+
+    if (isBalanced(expr)) {
+        cout << "Balanced parentheses ✅\n";
+    } else {
+        cout << "Not balanced ❌\n";
+    }
+
+    return 0;
+}
+
