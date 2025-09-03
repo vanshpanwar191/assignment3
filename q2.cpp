fde6ad2cbe477f4bec677cbfdc602bc32364f50a
vanshpanwar191 <vpanwar_be24@thapar.edu>
2025-09-03 08:51:07 +0530

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -1,36 +1,65 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-#define S 100;
-class stack{
-    char arr[S];
-    int top;
-    public:
-       stack(int i){
-         S=i;
-         top=-1;
-       }
-       bool isfull(){
-         return(top==S-1);
-       }
-       bool isempty(){
-         return(top==-1);
-       }
-       void push(){
-         if(isfull()){
-           cout<<"cant enter the string.\n";
-         }
-         else{
-         cout<<"enter the string:\n'";
-          cin>>arr[++top];
-         }
-       }
-       void display(){
-           for(int i=top;i>=0;i--){
-               cout<<arr[i];
-           }
-       }
-    
 
-    
-    
+#define MAX 100  
+
+class stack {
+    int size;       
+    char arr[MAX];  
+    int top;
+
+public:
+    stack(int n) {
+        size = n;
+        top = -1;
+    }
+
+    bool isfull() {
+        return (top == size - 1);
+    }
+
+    bool isempty() {
+        return (top == -1);
+    }
+
+    void pushString(string str) {
+        if (str.length() > size) {
+            cout << "String too long for stack!\n";
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            arr[++top] = str[i];
+        }
+        cout << "String pushed into stack.\n";
+    }
+
+    void display() {
+        if (isempty()) {
+            cout << "Stack is empty!\n";
+        } else {
+            cout << "Stack contents (top to bottom): ";
+            for (int i = top; i >= 0; i--) {
+                cout << arr[i];  
+            }
+            cout << "\n";
+        }
+    }
 };
+
+int main() {
+    int x;
+    cout << "Enter the size of string: ";
+    cin >> x;
+
+    stack s(x);
+
+    string str;
+    cout << "Enter the string: ";
+    cin >> str;
+
+    s.pushString(str);
+    s.display();
+
+    return 0;
+}
+
