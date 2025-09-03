9c13794d8a4c37d6cc0d79ad92f9586a050dd786
vanshpanwar191 <vpanwar_be24@thapar.edu>
2025-09-03 21:42:37 +0530

    Create q4.cpp

---
+++ q4.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <stack>
+#include <cctype>  
+using namespace std;
+
+
+int precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    if (op == '^') return 3; 
+    return 0;
+}
+
+string infixToPostfix(string infix) {
+    stack<char> s;
+    string postfix = "";
+
+    for (char ch : infix) {
+        
+        if (isalnum(ch)) {
+            postfix += ch;
+        }
+      
+        else if (ch == '(') {
+            s.push(ch);
+        }
+        
+        else if (ch == ')') {
+            while (!s.empty() && s.top() != '(') {
+                postfix += s.top();
+                s.pop();
+            }
+            s.pop(); 
+        }
+        
+        else {
+            while (!s.empty() && precedence(s.top()) >= precedence(ch)) {
+                postfix += s.top();
+                s.pop();
+            }
+            s.push(ch);
+        }
+    }
+
+  
+    while (!s.empty()) {
+        postfix += s.top();
+        s.pop();
+    }
+
+    return postfix;
+}
+
+int main() {
+    string infix;
+    cout << "Enter infix expression: ";
+    cin >> infix;
+
+    string postfix = infixToPostfix(infix);
+    cout << "Postfix expression: " << postfix << endl;
+
+    return 0;
+}
+
