ce86f43e2d380af625193292e3a8b63f64cbd452
vanshpanwar191 <vpanwar_be24@thapar.edu>
2025-09-03 08:37:50 +0530

    Create q2.cpp

---
+++ q2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class stack{
+    char arr[S];
+    int top;
+    public:
+       stack(int i){
+         S=i;
+         top=-1;
+       }
+       bool isfull(){
+         return(top==S-1);
+       }
+       bool isempty(){
+         return(top==-1);
+       }
+       void push(){
+         if(isfull()){
+           cout<<"cant enter the string.\n";
+         }
+         else{
+         cout<<"enter the string:\n'";
+          cin>>arr[++top];
+         }
+       }
+    
+
+    
+    
+};
